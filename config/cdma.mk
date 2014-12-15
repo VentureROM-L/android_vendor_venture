6d8b75eecaca05841f80b040a110372637a88397
Topazstix <robertdrone@ovi.com>
2014-12-14 20:35:46 -0700

    adding empty legacy branches

--- config/cdma.mk
+++
@@ -1,3 +0,0 @@
-# CDMA APN list
-PRODUCT_COPY_FILES += \
-    vendor/venture/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
--- config/common_tablet.mk
+++
@@ -1,8 +0,0 @@
-# Common settings and files
--include vendor/venture/config/common.mk
-
-# Add tablet overlays
-PRODUCT_PACKAGE_OVERLAYS += vendor/venture/overlay/common_tablet
-
-PRODUCT_CHARACTERISTICS := tablet
-
